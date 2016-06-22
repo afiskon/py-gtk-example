994b7141bbbdbc382fe10cbb0bcad7ae7cd8cb78
Aleksander Alekseev <afiskon@gmail.com>
2016-06-22 15:55:35 +0300

    PEP8

--- gtk-example.py
+++ gtk-example.py
@@ -20,28 +20,35 @@ ICON = os.path.join(CURRDIR, 'python3.xpm')
 # See:
 # * http://ubuntuforums.org/showthread.php?t=1923373#post11902222
 # * https://github.com/syncthing/syncthing-gtk/blob/master/syncthing_gtk/statusicon.py
+
+
 class TrayIcon:
-	def __init__(self, appid, icon, menu):
-		self.menu = menu
 
-		APPIND_SUPPORT = 1
-		try: from gi.repository import AppIndicator3
-		except: APPIND_SUPPORT = 0
+    def __init__(self, appid, icon, menu):
+        self.menu = menu
 
-		if APPIND_SUPPORT == 1:
-			self.ind = AppIndicator3.Indicator.new(appid, icon, AppIndicator3.IndicatorCategory.APPLICATION_STATUS)
-			self.ind.set_status (AppIndicator3.IndicatorStatus.ACTIVE)
-			self.ind.set_menu(self.menu)
-		else:
-			self.ind = Gtk.StatusIcon()
-			self.ind.set_from_file(icon)
-			self.ind.connect('popup-menu', self.right_click_event)
+        APPIND_SUPPORT = 1
+        try:
+            from gi.repository import AppIndicator3
+        except:
+            APPIND_SUPPORT = 0
 
-	def right_click_event(self, icon, button, time):
-		def pos(menu, aicon):
-			return (Gtk.StatusIcon.position_menu(menu, aicon))
+        if APPIND_SUPPORT == 1:
+            self.ind = AppIndicator3.Indicator.new(
+                appid, icon, AppIndicator3.IndicatorCategory.APPLICATION_STATUS)
+            self.ind.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
+            self.ind.set_menu(self.menu)
+        else:
+            self.ind = Gtk.StatusIcon()
+            self.ind.set_from_file(icon)
+            self.ind.connect('popup-menu', self.right_click_event)
 
-		self.menu.popup(None, None, pos, icon, button, time)
+    def right_click_event(self, icon, button, time):
+        def pos(menu, aicon):
+            return (Gtk.StatusIcon.position_menu(menu, aicon))
+
+        self.menu.popup(None, None, pos, icon, button, time)
+
 
 class Handler:
 
